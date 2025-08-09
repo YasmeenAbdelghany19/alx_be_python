a419f98ab76ac7c9bfb87302abe37ac0ca5e3ca3
YasmeenAbdelghany19 <abdelghany19@itu.edu.tr>
2025-08-09 22:49:40 +0300

    0. Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,28 @@
+"""Class Definition:
+
+Define a class named BankAccount.
+Use the __init__ method to initialize an account_balance attribute. Optionally, accept an initial balance parameter, defaulting to zero.
+Encapsulation and Behaviors:
+
+Implement deposit(amount), withdraw(amount), and display_balance() methods.
+deposit should add the specified amount to account_balance.
+withdraw should deduct the amount from account_balance if funds are sufficient, returning True; otherwise, return False and do not alter the balance.
+display_balance should print the current balance in a user-friendly format.
+""" 
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        self.account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance}")
+
